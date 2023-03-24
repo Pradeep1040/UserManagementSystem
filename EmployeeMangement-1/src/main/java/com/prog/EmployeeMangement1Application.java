c0e9085cf13052e435406463302c162da3a79f75
Pradeep <97354272+Pradeep1040@users.noreply.github.com>
2023-03-24 16:40:03 +0530

    Add files via upload

---
+++ EmployeeMangement-1/src/main/java/com/prog/EmployeeMangement1Application.java
@@ -0,0 +1,13 @@
+package com.prog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeMangement1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeMangement1Application.class, args);
+	}
+
+}
